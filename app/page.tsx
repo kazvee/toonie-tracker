02ac77d64928ca78faedb047fefcee8d8f56832b
kazvee <kazveedev@gmail.com>
2024-01-07 12:09:58 -0700

    Update main page content

--- app/page.tsx
+++ app/page.tsx
@@ -15,19 +15,21 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape} />
           <p
-            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
-          >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <strong>🪙🦆 Welcome to Toonie Tracker! 💰💸</strong>
+          </p>
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            This is a lightly modified version of the{' '}
+            <a href="https://nextjs.org/learn/" className="text-pink-500">
+              Learn Next.js Course{' '}
             </a>
-            , brought to you by Vercel.
+            brought to you by Vercel and one developer&apos;s love of emojis! 😃
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
+            <span>🪵 Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
