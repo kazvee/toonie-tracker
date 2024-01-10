d4eaf726111f7941a8df06b1c7dcaaefe4ea3ca6
kazvee <kazveedev@gmail.com>
2024-01-10 14:15:30 -0500

    Implement functionality to generate invoice creation date for new invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -19,6 +19,7 @@ export async function createInvoice(formData: FormData) {
     status: formData.get('status'),
   });
   const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
   // Test it out:
   console.log('😀', formData);
 }
