4d18f0c5327e7f438bf50c5b72f626d72194b99a
kazvee <kazveedev@gmail.com>
2024-01-16 13:45:04 -0700

    Implement password comparison with bcrypt during login

--- auth.ts
+++ auth.ts
@@ -4,14 +4,15 @@ import { authConfig } from './auth.config';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0];
   } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
+    console.error('🙁 Failed to fetch user:', error);
+    throw new Error('🙁 Failed to fetch user.');
   }
 }
 
@@ -28,8 +29,9 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
         }
-
+        console.log('🙁 Invalid credentials');
         return null;
       },
     }),
