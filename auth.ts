b6577ea08eb58a220427e9a2aef0594e43414813
kazvee <kazveedev@gmail.com>
2024-01-16 13:53:56 -0700

    Integrate authentication logic with login form and add error handling

--- auth.ts
+++ auth.ts
@@ -30,7 +30,10 @@ export const { auth, signIn, signOut } = NextAuth({
           const user = await getUser(email);
           if (!user) return null;
           const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
         }
+
         console.log('🙁 Invalid credentials');
         return null;
       },
